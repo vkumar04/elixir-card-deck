49c7849aefea80ea6ad650e3f34afc1b1f7f6120
Vick Kumar <vick.kumar44@gmail.com>
2018-03-12 20:22:53 -0400

    created create deck and shuffle functions

---
+++ lib/cards.ex
@@ -0,0 +1,14 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace","Two","Three","Four","Five"]
+    suites = ["Spades","Hearts","Diamonds","Clubs"]
+
+    for value <- values, suite <- suites do
+      "#{value} of #{suite}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+end
